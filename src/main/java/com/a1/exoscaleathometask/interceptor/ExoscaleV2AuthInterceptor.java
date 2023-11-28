291afabdbe878395e05742f4eb63c91a704dabca
zndavid <zndavid95@gmail.com>
2023-11-28 10:37:18 +0100

    Add missing logic to the interceptor

--- src/main/java/com/a1/exoscaleathometask/interceptor/ExoscaleV2AuthInterceptor.java
+++ src/main/java/com/a1/exoscaleathometask/interceptor/ExoscaleV2AuthInterceptor.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.util.ForwardedHeaderUtils;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.crypto.Mac;
@@ -16,6 +17,7 @@ import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Base64;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -27,7 +29,13 @@ public class ExoscaleV2AuthInterceptor implements ClientHttpRequestInterceptor {
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
         long expirationTs = Instant.now().getEpochSecond() + 10 * 60; // Expiration time set to 10 minutes
         String signature = generateSignature(request, body, expirationTs);
-        String authHeader = String.format("EXO2-HMAC-SHA256 credential=%s,expires=%d,signature=%s", apiProperties.key(), expirationTs, signature);
+        String signedQueryArgs = getSignedQueryArgs(request);
+        String authHeader = String.format("EXO2-HMAC-SHA256 credential=%s%s,expires=%d,signature=%s",
+                apiProperties.key(),
+                signedQueryArgs.isEmpty() ? "" : ",signed-query-args=" + signedQueryArgs,
+                expirationTs,
+                signature
+        );
         request.getHeaders().add(HttpHeaders.AUTHORIZATION, authHeader);
         return execution.execute(request, body);
     }
@@ -38,29 +46,41 @@ public class ExoscaleV2AuthInterceptor implements ClientHttpRequestInterceptor {
             SecretKeySpec secretKeySpec = new SecretKeySpec(apiProperties.secret().getBytes(StandardCharsets.UTF_8), HMAC_SHA256);
             mac.init(secretKeySpec);
 
-            //Obtain method name
             String method = request.getMethod().toString();
-            //Obtain uri
             URI uri = request.getURI();
-            //Obtain path
             String path = uri.getPath();
-
-            String queryParams = UriComponentsBuilder.fromUri(uri).build().getQueryParams()
-                    .entrySet().stream()
-                    .sorted((e1, e2) -> e1.getKey().compareToIgnoreCase(e2.getKey()))
-                    .map(entry -> String.join(";", entry.getValue()))
-                    .collect(Collectors.joining());
+            String queryParams = getSortedQueryParams(uri);
 
             String message = String.join("\n",
                     method + " " + path,
-                    new String(body, StandardCharsets.UTF_8),
+                    body != null ? new String(body, StandardCharsets.UTF_8) : "",
                     queryParams,
-                    "",
+                    "",    //The official website (https://openapi-v2.exoscale.com/) says: "Request header values, concatenated without separator (none at the moment, leave empty)
                     String.valueOf(expirationTs));
             byte[] hash = mac.doFinal(message.getBytes(StandardCharsets.UTF_8));
             return Base64.getEncoder().encodeToString(hash);
         } catch (Exception e) {
             throw new IOException("Failed to generate signature", e);
         }
+    }
+
+    private String getSortedQueryParams(URI uri) {
+        return UriComponentsBuilder.fromUri(uri).build().getQueryParams()
+                .entrySet()
+                .stream()
+                .sorted((e1, e2) -> e1.getKey().compareToIgnoreCase(e2.getKey()))
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining());
+    }
+
+    private String getSignedQueryArgs(HttpRequest request) {
+        return ForwardedHeaderUtils.adaptFromForwardedHeaders(request.getURI(), request.getHeaders())
+                .build()
+                .getQueryParams()
+                .entrySet()
+                .stream()
+                .sorted((e1, e2) -> e1.getKey().compareToIgnoreCase(e2.getKey()))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.joining(";"));
     }
 }
